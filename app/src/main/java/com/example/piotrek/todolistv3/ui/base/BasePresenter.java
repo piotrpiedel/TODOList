f44ffb0fa8c6b66a77471379aa246c4a0319c2c7
piotrpiedel <piotrpiedel96@gmail.com>
2018-06-06 20:17:43 +0200

    Update BasePresenter.java

--- app/src/main/java/com/example/piotrek/todolistv3/ui/base/BasePresenter.java
+++ app/src/main/java/com/example/piotrek/todolistv3/ui/base/BasePresenter.java
@@ -2,9 +2,7 @@ package com.example.piotrek.todolistv3.ui.base;
 
 
 public class BasePresenter<V> {
-        // BasePresenter<CategoryContract.View>
         protected V mView;
-//    CategoryContract.View mView;
     public void attachView(V view) {
         this.mView = view;
     }
